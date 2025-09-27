daf59e15fb3982c101b39fb0d48be7194678f081
Ananãsio <102226758+aaanananasss@users.noreply.github.com>
2025-09-28 00:19:28 +0300

    Update Object.py

--- Object.py
+++ Object.py
@@ -1,22 +1,14 @@
 from __future__ import annotations
-from typing import Sequence
 
-class Object:
-    def __init__(self):
-        """Абстрактный класс визуализации объектов."""
-        pass
+class Object: pass
 
-class UI(Object):
-    """Полноценные UI-элементы."""
+class UI(Object): pass
 
 class Text(UI):
     def __init__(self,
         content: str = '',
         font: Font = None,
     ) -> None:
-        """
-        Текст.
-        """
         self.content = content
         self.font = font
 
@@ -24,15 +16,6 @@ class Text(UI):
         def __init__(self,
             file: str,
             size: int = 12,
-            styles: Sequence[str] | str = None,
-            direction: str = 'lr',
-            interval: Sequence[int, int] = 1, #интервал межбуквенный, межстрочный
-            break_words: bool = False,
         ) -> None:
-            """Шрифт текста."""
             self.file = file
             self.size = size
-            self.styles = styles
-            self.direction = direction
-            self.interval = interval
-            self.break_words = break_words
